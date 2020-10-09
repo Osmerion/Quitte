0845fc59eb54283da0571ddcc6d0a6b91ce6d1be
Leon Linhart <themrmilchmann@gmail.com>
2020-10-09 11:56:56 +0200

    feat(quitte): basic ObservableDeque tests

---
+++ modules/quitte/src/test/java/com/osmerion/quitte/collections/MockDequeChangeListener.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (c) 2018-2019 Leon Linhart,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.osmerion.quitte.collections;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import javax.annotation.Nullable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+final class MockDequeChangeListener<E> implements DequeChangeListener<E> {
+
+    @Nullable
+    private Context context;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onChanged(DequeChangeListener.Change<? extends E> change) {
+        if (this.context == null) return;
+
+        change.getLocalChanges().stream()
+            .map(it -> {
+                if (it instanceof DequeChangeListener.LocalChange.Insertion) {
+                    DequeChangeListener.LocalChange.Insertion<E> localChange = ((DequeChangeListener.LocalChange.Insertion<E>) it);
+                    return new Operation(OpType.INSERTION, localChange.getSite(), localChange.getElements());
+                } else if (it instanceof DequeChangeListener.LocalChange.Removal) {
+                    DequeChangeListener.LocalChange.Removal<E> localChange = ((DequeChangeListener.LocalChange.Removal<E>) it);
+                    return new Operation(OpType.REMOVAL, localChange.getSite(), localChange.getElements());
+                } else {
+                    throw new UnsupportedOperationException();
+                }
+            })
+            .forEach(this.context.operations::add);
+    }
+
+    public Context push() {
+        if (this.context != null) throw new IllegalStateException();
+        return (this.context = new Context());
+    }
+
+    public final class Context implements AutoCloseable {
+
+        private final ArrayDeque<Operation> operations = new ArrayDeque<>();
+
+        private Context() {}
+
+        /** Closes this context and asserts that all operations have been consumed. */
+        @Override
+        public void close() {
+            MockDequeChangeListener.this.context = null;
+            this.assertEmpty();
+        }
+
+        public void assertInsertion(Site site, E element) {
+            this.assertInsertion(site, List.of(element));
+        }
+
+        public void assertInsertion(Site site, List<? extends E> elements) {
+            Operation operation = this.operations.pollFirst();
+
+            assertNotNull(operation);
+            assertEquals(OpType.INSERTION, operation.type);
+            assertEquals(site, operation.site);
+            assertEquals(elements, operation.elements);
+        }
+
+        public void assertRemoval(Site site, E element) {
+            this.assertRemoval(site, List.of(element));
+        }
+
+        public void assertRemoval(Site site, List<? extends E> elements) {
+            Operation operation = this.operations.pollFirst();
+
+            assertNotNull(operation);
+            assertEquals(OpType.REMOVAL, operation.type);
+            assertEquals(site, operation.site);
+            assertEquals(elements, operation.elements);
+        }
+
+        /** Asserts that there are no unconsumed operations left in the current context. */
+        public void assertEmpty() {
+            assertTrue(this.operations.isEmpty());
+        }
+
+    }
+
+    private final class Operation {
+
+        private final OpType type;
+        @Nullable private final Site site;
+        private final List<? extends E> elements;
+
+        Operation(OpType type, @Nullable Site site, List<? extends E> elements) {
+            this.type = type;
+            this.site = site;
+            this.elements = elements;
+        }
+
+    }
+
+    enum OpType {
+        INSERTION,
+        REMOVAL
+    }
+
+}
---
+++ modules/quitte/src/test/java/com/osmerion/quitte/collections/ObservableDequeTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2018-2020 Leon Linhart,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.osmerion.quitte.collections;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * TODO doc
+ *
+ * @author  Leon Linhart
+ */
+@SuppressWarnings("ConstantConditions")
+public final class ObservableDequeTest {
+
+    private Deque<String> underlyingDeque;
+    private ObservableDeque<String> observableDeque;
+    private MockDequeChangeListener<String> changeListener;
+
+    @BeforeEach
+    public void reset() {
+        this.observableDeque = ObservableDeque.of(this.underlyingDeque = new ArrayDeque<>());
+        this.observableDeque.addListener(this.changeListener = new MockDequeChangeListener<>());
+    }
+
+    @Test
+    @DisplayName("ObservableSet#clear()")
+    public void testClear() {
+        try (var changeCtx = this.changeListener.push()) {
+            this.observableDeque.addAll(List.of("foo", "bar"));
+            changeCtx.assertInsertion(DequeChangeListener.Site.TAIL, List.of("foo", "bar"));
+            changeCtx.assertEmpty();
+
+            this.observableDeque.add("foo");
+            changeCtx.assertInsertion(DequeChangeListener.Site.TAIL, "foo");
+
+            this.observableDeque.clear();
+            changeCtx.assertRemoval(DequeChangeListener.Site.OPAQUE, List.of("foo", "bar", "foo"));
+            changeCtx.assertEmpty();
+        }
+    }
+
+    @Test
+    @DisplayName("ObservableSet#isEmpty() after modification")
+    public void testIsEmpty() {
+        this.observableDeque.add("foo");
+        assertFalse(this.observableDeque.isEmpty());
+        assertFalse(this.underlyingDeque.isEmpty());
+
+        this.observableDeque.clear();
+        assertTrue(this.observableDeque.isEmpty());
+        assertTrue(this.underlyingDeque.isEmpty());
+    }
+
+}
