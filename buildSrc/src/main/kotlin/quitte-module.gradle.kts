c9d4107fd30dfa61386e1b09c0d306d1bb1ab0bb
Leon Linhart <themrmilchmann@gmail.com>
2021-03-03 21:40:07 +0100

    build: update JUnit {5.6.2 => 5.7.1}

--- buildSrc/src/main/kotlin/quitte-module.gradle.kts
+++ buildSrc/src/main/kotlin/quitte-module.gradle.kts
@@ -140,5 +140,5 @@ dependencies {
     compileOnly(group = "com.google.code.findbugs", name = "jsr305", version = "3.0.2")
     testCompileOnly(group = "com.google.code.findbugs", name = "jsr305", version = "3.0.2")
 
-    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.6.2")
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.7.1")
 }
