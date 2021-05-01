2c9daf6f99659d2757083b23545fdcf526ac7af0
Leon Linhart <themrmilchmann@gmail.com>
2021-05-01 14:50:16 +0200

    feat(quitte): ObservableList tests

---
+++ modules/quitte/src/test/java/com/osmerion/quitte/collections/MockListChangeListener.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2018-2021 Leon Linhart,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.osmerion.quitte.collections;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import javax.annotation.Nullable;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+final class MockListChangeListener<E> implements CollectionChangeListener<ObservableList.Change<? extends E>> {
+
+    @Nullable
+    private Context context;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onChanged(ObservableList.Change<? extends E> change) {
+        if (this.context == null) return;
+
+        if (change instanceof ObservableList.Change.Permutation) {
+            ObservableList.Change.Permutation<E> permutation = (ObservableList.Change.Permutation<E>) change;
+            this.context.operations.add(new Permutation<>(permutation.getIndices()));
+        } else if (change instanceof ObservableList.Change.Update) {
+            ObservableList.Change.Update<E> update = (ObservableList.Change.Update<E>) change;
+            update.getLocalChanges().forEach(localChange -> {
+                OpType type;
+
+                if (localChange instanceof ObservableList.LocalChange.Insertion) {
+                    type = OpType.INSERTION;
+                } else if (localChange instanceof ObservableList.LocalChange.Removal) {
+                    type = OpType.REMOVAL;
+                } else if (localChange instanceof ObservableList.LocalChange.Update) {
+                    type = OpType.UPDATE;
+                } else {
+                    throw new IllegalStateException();
+                }
+
+                this.context.operations.add(new Update<>(type, localChange.getIndex(), localChange.getElements()));
+            });
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    public Context push() {
+        if (this.context != null) throw new IllegalStateException();
+        return (this.context = new Context());
+    }
+    
+    public final class Context implements AutoCloseable {
+
+        private final ArrayDeque<Operation<E>> operations = new ArrayDeque<>();
+
+        private Context() {}
+
+        /** Closes this context and asserts that all operations have been consumed. */
+        @Override
+        public void close() {
+            MockListChangeListener.this.context = null;
+            this.assertEmpty();
+        }
+
+        @SafeVarargs
+        public final void assertInsertion(int offset, E... elements) {
+            assertInsertion(offset, List.of(elements));
+        }
+
+        public void assertInsertion(int offset, List<E> elements) {
+            Operation<E> operation = this.operations.pollFirst();
+
+            if (!(operation instanceof Update<E> update) || update.type != OpType.INSERTION) {
+                fail();
+                return;
+            }
+
+            assertEquals(offset, update.offset);
+            assertEquals(elements, update.elements);
+        }
+
+        @SafeVarargs
+        public final void assertRemoval(int offset, E... elements) {
+            assertRemoval(offset, List.of(elements));
+        }
+
+        public void assertRemoval(int offset, List<E> elements) {
+            Operation<E> operation = this.operations.pollFirst();
+
+            if (!(operation instanceof Update<E> update) || update.type != OpType.REMOVAL) {
+                fail();
+                return;
+            }
+
+            assertEquals(offset, update.offset);
+            assertEquals(elements, update.elements);
+        }
+
+        public void assertPermutation(List<Integer> indices) {
+            Operation<E> operation = this.operations.pollFirst();
+
+            if (!(operation instanceof Permutation<E> permutation)) {
+                fail();
+                return;
+            }
+
+            assertEquals(indices, permutation.indices);
+        }
+
+        /** Asserts that there are no unconsumed operations left in the current context. */
+        public void assertEmpty() {
+            assertTrue(this.operations.isEmpty());
+        }
+        
+    }
+
+    private interface Operation<E> {}
+
+    private static record Permutation<E>(List<Integer> indices) implements Operation<E> {}
+
+    private record Update<E>(OpType type, int offset, List<E> elements) implements Operation<E> {}
+
+    enum OpType {
+        INSERTION,
+        REMOVAL,
+        UPDATE
+    }
+
+}
---
+++ modules/quitte/src/test/java/com/osmerion/quitte/collections/ObservableListTest.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright (c) 2018-2021 Leon Linhart,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.osmerion.quitte.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for {@link ObservableList} and bundled implementations.
+ *
+ * @author  Leon Linhart
+ */
+@SuppressWarnings("ConstantConditions")
+public final class ObservableListTest {
+
+    private List<String> underlyingList;
+    private ObservableList<String> observableList;
+    private MockListChangeListener<String> changeListener;
+
+    @BeforeEach
+    public void reset() {
+        this.observableList = ObservableList.of(this.underlyingList = new ArrayList<>());
+        this.observableList.addListener(this.changeListener = new MockListChangeListener<>());
+    }
+
+    @Test
+    @DisplayName("ObservableList#clear()")
+    public void testClear() {
+        try (var changeCtx = this.changeListener.push()) {
+            this.observableList.addAll(List.of("foo", "bar"));
+            changeCtx.assertInsertion(0, "foo");
+            changeCtx.assertInsertion(1, "bar");
+            changeCtx.assertEmpty();
+
+            this.observableList.add("foo");
+            changeCtx.assertInsertion(2, "foo");
+            changeCtx.assertEmpty();
+
+            this.observableList.clear();
+            changeCtx.assertRemoval(0, "foo");
+            changeCtx.assertRemoval(0, "bar");
+            changeCtx.assertRemoval(0, "foo");
+            changeCtx.assertEmpty();
+        }
+    }
+
+    @Test
+    @DisplayName("ObservableSet#isEmpty() after modification")
+    public void testIsEmpty() {
+        this.observableList.add("foo");
+        assertFalse(this.observableList.isEmpty());
+        assertFalse(this.underlyingList.isEmpty());
+
+        this.observableList.clear();
+        assertTrue(this.observableList.isEmpty());
+        assertTrue(this.underlyingList.isEmpty());
+    }
+
+    @Test
+    @DisplayName("ObservableSet#isEmpty() after modification of underlying Set")
+    public void testIsEmptyWithUnderlyingModification() {
+        this.underlyingList.add("foo");
+        assertFalse(this.observableList.isEmpty());
+
+        this.underlyingList.clear();
+        assertTrue(this.observableList.isEmpty());
+    }
+
+    @Test
+    @DisplayName("ObservableList addition/removal of `null`")
+    public void testNull() {
+        try (var changeCtx = this.changeListener.push()) {
+            this.observableList.add(null);
+            assertEquals(1, this.underlyingList.size());
+            assertEquals(1, this.observableList.size());
+            changeCtx.assertInsertion(0, Collections.singletonList(null));
+            changeCtx.assertEmpty();
+
+            this.observableList.remove(null);
+            assertEquals(0, this.underlyingList.size());
+            assertEquals(0, this.observableList.size());
+            changeCtx.assertRemoval(0, Collections.singletonList(null));
+            changeCtx.assertEmpty();
+        }
+    }
+
+    @Test
+    @DisplayName("ObservableList permutation detection")
+    public void testPerm() {
+        try (var changeCtx = this.changeListener.push()) {
+            this.observableList.addAll(List.of("foo", "bar", "blub", "dup"));
+            changeCtx.assertInsertion(0, "foo");
+            changeCtx.assertInsertion(1, "bar");
+            changeCtx.assertInsertion(2, "blub");
+            changeCtx.assertInsertion(3, "dup");
+            changeCtx.assertEmpty();
+
+            this.observableList.sort(Comparator.naturalOrder());
+            changeCtx.assertPermutation(List.of(3, 0, 1, 2));
+            changeCtx.assertEmpty();
+        }
+    }
+
+    @Test
+    @DisplayName("ObservableSet#setAll()")
+    public void testSetAll() {
+        List<String> elements = List.of("A", "b");
+        ObservableList<String> observableList = ObservableList.of(new ArrayList<>(elements));
+        assertFalse(observableList.setAll(elements));
+    }
+
+    @Test
+    @DisplayName("ObservableList#size()")
+    public void testSize() {
+        assertEquals(0, this.underlyingList.size());
+        assertEquals(0, this.observableList.size());
+
+        this.observableList.add("foo");
+        assertEquals(1, this.observableList.size());
+
+        this.observableList.add("foo");
+        assertEquals(2, this.observableList.size());
+
+        this.observableList.remove("foo");
+        assertEquals(1, this.underlyingList.size());
+        assertEquals(1, this.observableList.size());
+    }
+
+    @Test
+    @DisplayName("ObservableList#size() after modification of underlying List")
+    public void testSizeWithUnderlyingModification() {
+        assertEquals(0, this.underlyingList.size());
+        assertEquals(0, this.observableList.size());
+
+        this.underlyingList.add("foo");
+        assertEquals(1, this.observableList.size());
+
+        this.observableList.add("foo"); // Using observableList on purpose to check consistency
+        assertEquals(2, this.observableList.size());
+
+        this.underlyingList.remove("foo");
+        assertEquals(1, this.underlyingList.size());
+        assertEquals(1, this.observableList.size());
+    }
+
+
+}
